61cf46b4d7d524bb28bf16778f5226e67e2eab19
salonebi <booksalonebi@gmail.com>
2025-07-09 17:08:07 +0400

    config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // ... your other configs might be here
+   output: 'export',
 
   // Add this block to disable ESLint during builds
   eslint: {
