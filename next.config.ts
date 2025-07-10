4fa9c54957c54114cd7229fa153e06190a07e193
salonebi <booksalonebi@gmail.com>
2025-07-10 21:18:42 +0400

    app hosting init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // ... your other configs might be here
-   output: 'export',
 
   // Add this block to disable ESLint during builds
   eslint: {
